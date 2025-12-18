706bd8632a17960a32bce224ae7e0e27fba29d30
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-12-18 12:55:15 +0545

    .MAUI Blazor Tasks Completed

---
+++ Components/Pages/Calculator.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop8.Pages
+{
+    public partial class Calculator
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double Result { get; set; }
+
+        public void Add()
+        {
+            Result = Number1 + Number2;
+        }
+
+        public void Subtract()
+        {
+            Result = Number1 - Number2;
+        }
+
+        public void Multiply()
+        {
+            Result = Number1 * Number2;
+        }
+
+        public void Divide()
+        {
+            Result = Number2 != 0 ? Number1 / Number2 : 0;
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop8.Models
+{
+    public class Product
+    {
+        public required string Name { get; set; }
+        public required string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Likes { get; set; }
+    }
+}
+
